c9a4a3ce2b4988c7f77de4000b10dd32186f916f
Josh <jokajak@gmail.com>
2023-10-13 15:55:57 -0400

    chore: add minio bitwarden entry

--- terraform/bitwarden/main.tf
+++ terraform/bitwarden/main.tf
@@ -1,3 +1,17 @@
 resource "bitwarden_folder" "kubernetes_credentials" {
   name = "Kubernetes Credentials"
 }
+
+resource "random_password" "minio_password" {
+  length           = 32
+  special          = true
+  override_special = "_=+-,~"
+}
+
+resource "bitwarden_item_login" "minio" {
+  folder_id = bitwarden_folder.kubernetes_credentials.id
+
+  name     = "minio credentials"
+  username = "Recoil7901"
+  password = random_password.minio_password.result
+}
