8aed51adc15999ae184ba650a840770259a6bd32
Josh <jokajak@gmail.com>
2023-12-02 14:57:29 -0500

    feat(authentik): try managing authentication flow

--- terraform/authentik/main.tf
+++ terraform/authentik/main.tf
@@ -21,3 +21,36 @@ resource "authentik_source_oauth" "github" {
   consumer_key    = data.bitwarden_item_login.oidc_creds.username
   consumer_secret = data.bitwarden_item_login.oidc_creds.password
 }
+
+################################################################################
+# Flow management
+#
+# The intention is to allow login with social logins like github and google
+################################################################################
+# Create identification stage with sources and showing a password field
+
+data "authentik_source" "inbuilt" {
+  managed = "goauthentik.io/sources/inbuilt"
+}
+
+data "authentik_flow" "default-authentication-flow" {
+  slug = "default-authentication-flow"
+}
+
+resource "authentik_stage_password" "inbuilt" {
+  name     = "show-password-field"
+  backends = ["authentik.core.auth.InbuiltBackend"]
+}
+
+resource "authentik_stage_identification" "name" {
+  name           = "show-social-logins"
+  user_fields    = ["username"]
+  sources        = [data.authentik_source.inbuilt.uuid, authentik_source_oauth.github.uuid]
+  password_stage = authentik_stage_password.builtin.id
+}
+
+resource "authentik_flow_stage_binding" "identification" {
+  target = data.authentik_flow.default-authentication-flow.uuid
+  stage  = authentik_stage_identification.name.id
+  order  = 10
+}
